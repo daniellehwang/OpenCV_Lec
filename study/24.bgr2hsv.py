2b976de4d626ba7a1c3306eb011f30e4eb25233c
DokyungHwang <ghkdehrud@gmail.com>
2021-10-06 17:57:17 +0900

    10/6

---
+++ study/24.bgr2hsv.py
@@ -0,0 +1,9 @@
+import cv2
+
+src = cv2.imread("../img/crow.jpg")
+dst = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
+
+cv2.imshow("src", src)
+cv2.imshow("dst", dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
